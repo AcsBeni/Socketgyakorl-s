127d152d8a3cfd08866835d0a51d4763148ca1fd
Acsbeni <acsbenjamin@turr.hu>
2025-12-17 12:12:17 +0100

    Felhasználó be/ki lépés

--- public/client.js
+++ public/client.js
@@ -1,19 +1,38 @@
 const socket = io();
-const msgInput = document.getElementById('msgInput');
-const sendBtn = document.getElementById('sendBtn');
-const messagesBox = document.getElementById('messages');
+const config = window.CHAT_CONFIG;
 
-sendBtn.onclick=()=>{
-    if(msgInput.value != ''){
-        socket.emit('newMessage',msgInput.value)
-        msgInput.value = ''
-    }
+const leaveRoomBtn = document.getElementById('leaveRoom');
+const messages = document.getElementById('messages');
 
-}
-socket.on('newMessage', (msg)=>{
-    const div = document.createElement('div')
-    div.innerText = msg;
-    messagesBox.appendChild(div)
-    messagesBox.scrollTop = messagesBox.scrollHeight;
+const renderMessage = ({ nickname, message, timestamp, type }) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <div class="${type}">
+            <strong>${nickname}</strong>
+            <span>${timestamp}</span>
+            <p>${message}</p>
+        </div>
+    `;
+    messages.appendChild(li);
+};
 
-})
+leaveRoomBtn.addEventListener('click', () => {
+    socket.emit('leave-room', {
+        nickname: config.nickname,
+        roomId: config.roomId
+    });
+    window.location.href = `/?nickname=${config.nickname}`;
+});
+socket.emit('join-room', {
+    nickname: config.nickname,
+    roomId: config.roomId
+});
+
+socket.on('system-message', (text) => {
+    renderMessage({
+        nickname: 'SYSTEM',
+        message: text,
+        timestamp: new Date().toLocaleTimeString(),
+        type: 'system'
+    });
+});
--- server.js
+++ server.js
@@ -5,22 +5,61 @@ const io = require('socket.io')(server);
 const ejs = require('ejs');
 const path = require('path')
 
+const ROOMS = [
+    {id: "frontend", label: "Frontend programozás"},
+    {id: "backend", label: "Backend programozás"},
+    {id: "desktop", label: "Asztali alkalmazás fejlesztés"},
+    {id: "database", label: "Adatbázis kezelés"},
+    {id: "others", label: "egyéb témák"},
+]
+const ERRORS ={
+    missingFields: "Hiányzó adatok"
+}
+
+
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const {error = '',nickname = '', room = ''} = req.query
+    res.render('index',{rooms: ROOMS, error: ERRORS[error],nickname,room});
+})
+app.get('/main', (req, res) => {
+    const {nickname, room} = req.query
+    if(!nickname || !room){
+        return res.redirect(`/?error=missingFields&nickname=${nickname}&room=${room}`)
+    }
+    const chatConfig={
+        nickname,
+        roomId: room,
+        roomLabel: getRoomById(room)
+    }
+    res.render('main', {chatConfig});
 })
 
+const getRoomById = (roomId)=>{return ROOMS.find((room) => room.id === roomId).label}
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     console.log(`Új felhasználó csatlakozott: ${socket.id}`);
 
-    socket.on('newMessage', (msg)=> {
-        io.emit('newMessage', msg)
+    socket.on('join-room', ({ nickname, roomId }) => {
+        socket.join(roomId);
+
+        socket.to(roomId).emit(
+            'system-message',
+            `${nickname} csatlakozott a beszélgetéshez`
+        );
+    });
+    socket.on('leave-room', ({nickname, roomId})=>{
+         socket.to(roomId).emit(
+            'system-message',
+            `${nickname} kilépett a beszélgetésből`
+        );
+        socket.leave(roomId)
     })
 });
+
 
 server.listen(3000, ()=>{
     console.log('http://localhost:3000')
